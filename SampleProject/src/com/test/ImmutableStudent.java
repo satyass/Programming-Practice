4bd173d7442a21ae5313ba759cc4453a1785a34d
satyss <naidu.singam@yahoo.com>
2015-09-05 00:32:23 -0700

    Immutble class

---
+++ SampleProject/src/com/test/ImmutableStudent.java
@@ -0,0 +1,24 @@
+package com.test;
+
+public final class ImmutableStudent {
+	
+	final int id;
+	final String name;
+	
+	public ImmutableStudent(int id, String name){
+		this.id = id;
+		this.name = name;		
+	}
+
+	public static void main(String[] args) {		
+		ImmutableStudent is = new ImmutableStudent(10, "Satya");
+		
+		System.out.println("Id = "+is.id);
+		System.out.println("Name ="+is.name);
+		
+		ImmutableStudent is1 = new ImmutableStudent(20, "Uma");
+		
+		
+	}
+
+}
